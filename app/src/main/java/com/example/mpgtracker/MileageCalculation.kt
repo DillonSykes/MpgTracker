0bbacd3b63ed6dfe6da60760e6309e558049f4bb
DillonSykes <dsykes321@gmail.com>
2019-07-18 00:49:03 -0400

    init

---
+++ app/src/main/java/com/example/mpgtracker/MileageCalculation.kt
@@ -0,0 +1,12 @@
+package com.example.mpgtracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MileageCalculation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mileage_calculation)
+    }
+}
